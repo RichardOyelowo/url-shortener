aa04b6cd7ee59bae7bc14189d1ea8b614e86b75f
Richard <richardadebowale.oye@gmail.com>
2026-02-25 13:06:36 -0500

    updated database use

--- app/database.py
+++ app/database.py
@@ -1,18 +1,22 @@
 import os
+from typing import Annotated
+from fastapi import Depends
 from dotenv import load_dotenv
 from sqlalchemy.orm import DeclarativeBase
-from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker
+from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine, async_sessionmaker
 
 
 load_dotenv()
-DATABASE = os.getenv("DATABASE_URL")
 
 class Base(DeclarativeBase):
     pass
 
-engine = create_async_engine(DATABASE)
+engine = create_async_engine(os.getenv("DATABASE_URL"))
 async_session = async_sessionmaker(engine)
 
-async def get_db():
+async def get_session():
     async with async_session() as session:
         yield session
+
+
+SessionDep = Annotated[AsyncSession, Depends(get_session)]
