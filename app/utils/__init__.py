9e75e9b3f54e54378d69e82b528d692cc7b7fbbd
Richard <richardadebowale.oye@gmail.com>
2026-02-26 18:24:12 -0500

    added header verification for admin routes

--- app/utils/__init__.py
+++ app/utils/__init__.py
@@ -1,1 +1,2 @@
+from .auth import verify_header
 from .shortener import convert_to_shortcode, decode_shortcode
--- app/utils/auth.py
+++ app/utils/auth.py
@@ -1,1 +1,7 @@
+from fastapi import Header, HTTPException
+import os
+
+async def verify_header(x_admin_key: str = Header()):
+    if x_admin_key != os.getenv("SECRET_KEY"):
+        raise HTTPException(status_code=403, detail="forbidden")
 
