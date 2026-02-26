4bbb50fbf346143c634cc3c312e49e9a373e4bff
Richard <richardadebowale.oye@gmail.com>
2026-02-26 17:51:02 -0500

    admin route without headers

--- app/routers/admin.py
+++ app/routers/admin.py
@@ -1,31 +1,33 @@
-from os import link
-from app.models import Link, Click
-from sqlalchemy import select, delete
-from app.database import SessionDep
 from fastapi import APIRouter
+from app.models import Link, Click
+from app.database import SessionDep
+from sqlalchemy import select, delete
+from app.utils import decode_shortcode
+from app.schemas import LinkCreate, LinkResponse
 
-admin_router = APIRouter()
-
-
-@admin_router.get("/admins/links/")
-async def links(db: SessionDep):
-    results = await db.execute(select(Link))
-    links = results.scalars().all()
-
-    return {"links": links}
+admin_router = APIRouter(prefix="/admin")
 
 
-@admin_router.get("/admins/links/{shortcode}/analytics")
-async def analytics(shortcode: str, db: SessionDep):
-    results = await db.execute(select(Click).join(Click.link).where(Link.short_code == shortcode))
-    analytics = results.scalars().all()
+@admin_router.get("/links")
+async def all_links(db: SessionDep):
+    links = await db.execute(select(Link))
+    results = links.scalars().all()
+
+    return {"links": results}
+
+
+@admin_router.get("/links/{shortcode}/analytics/")
+async def get_analytics(shortcode: str, db: SessionDep):
+    result = await db.execute(select(Click).join(Link).where(Link.short_code == shortcode))
+    analytics = result.scalars().all()
 
     return {"analytics": analytics}
 
 
-@admin_router.delete("/admins/links/{id}")
-async def delete_link(id: int, db: SessionDep):
-    result = await db.execute(delete(Link).where(Link.id == id))
+@admin_router.delete("/links/{id}")
+async def delete_link(id: int, db : SessionDep):
+    await db.execute(delete(Link).where(Link.id == id))
     await db.commit()
-    
-    return {"message":"link deleted"}
+
+    return {"message": "Link deleted"}
+
--- app/routers/links.py
+++ app/routers/links.py
@@ -1,27 +1,42 @@
-from app.models import Link
-from fastapi import APIRouter
+from os import wait
+from app.models import Link, Click
 from sqlalchemy import select
 from app.schemas import LinkCreate
 from app.database import SessionDep 
 from app.utils import convert_to_shortcode
+from fastapi import APIRouter, HTTPException
+from fastapi.responses import RedirectResponse
 
 
 router = APIRouter()
 
-@router.get("/{shortcode}")
+@router.get("/links/{shortcode}")
 async def load_link(shortcode: str, db: SessionDep):
     results = await db.execute(select(Link).where(Link.short_code == shortcode))
     link = results.scalars().first()
-    return {"link": link}
+    
+    if link:
+        # create click object
+        clicked = Click(link_id=link.id)
+        db.add(clicked)
+        
+        # increment click count
+        link.click_count += 1
+        await db.commit()
+        await db.refresh(link)
+
+        return RedirectResponse(url=link.original_url, status_code=302)
+
+    raise HTTPException(status_code=404, detail="Link not Found")
 
 
 @router.post("/links/")
 async def create_link(link: LinkCreate, db: SessionDep):
     results = await db.execute(select(Link).where(Link.original_url == link.original_url))
-    existing = results.first()
+    existing = results.scalars().first()
 
     if existing:
-        return {"link": existing}
+        return existing
 
     new_link = Link(original_url=link.original_url)
     db.add(new_link)
