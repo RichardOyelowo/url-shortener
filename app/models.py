f24107373a63e4fdd61db012c07bc9757b35eafb
Richard <richardadebowale.oye@gmail.com>
2026-02-25 23:34:20 -0500

    updated Link.short_code to Optional

--- app/models.py
+++ app/models.py
@@ -1,6 +1,7 @@
 from .database import Base
+from typing import Optional
 from datetime import datetime, timezone
-from sqlalchemy import ForeignKey, String, Integer, ForeignKey
+from sqlalchemy import ForeignKey, String, Integer, ForeignKey, DateTime
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 
@@ -9,9 +10,9 @@ class Link(Base):
 
     id: Mapped[int] = mapped_column("id", Integer, primary_key=True)
     original_url: Mapped[str] = mapped_column("original_url", String)
-    short_code: Mapped[str] = mapped_column("short_code", String, unique=True)
+    short_code: Mapped[Optional[str]] = mapped_column("short_code", String, unique=True)
     click_count: Mapped[int] = mapped_column("click_count",Integer, default=0, nullable=False)
-    created_at: Mapped[datetime] = mapped_column("created_at", default=lambda: datetime.now(timezone.utc))
+    created_at: Mapped[datetime] = mapped_column("created_at",DateTime(timezone=True), default=lambda: datetime.now(timezone.utc))
     
     clicks: Mapped[list["Click"]] = relationship(back_populates="link")
 
@@ -24,6 +25,6 @@ class Click(Base):
 
     id: Mapped[int] = mapped_column("id", Integer, primary_key=True)
     link_id: Mapped[int] = mapped_column("link_id", Integer, ForeignKey("link.id"))
-    created_at: Mapped[datetime] = mapped_column("created_at", default=lambda: datetime.now(timezone.utc))
+    created_at: Mapped[datetime] = mapped_column("created_at",DateTime(timezone=True), default=lambda: datetime.now(timezone.utc))
     
     link: Mapped["Link"] = relationship(back_populates="clicks")
