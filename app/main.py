e0ff538dacba4f6bbb3a7af352e819a2e9247b4b
Richard <richardadebowale.oye@gmail.com>
2026-02-26 21:53:36 -0500

    mounted the css

--- app/main.py
+++ app/main.py
@@ -1,14 +1,18 @@
 from app.utils import verify_header
-from fastapi import FastAPI, Depends
+from app.templates_config import templates
+from fastapi.staticfiles import StaticFiles
 from app.routers import router, admin_router
+from fastapi import FastAPI, Depends, Request
 
 
 app = FastAPI()
+
+app.mount("/static", StaticFiles(directory="app/static"), name="static")
 
 app.include_router(admin_router, dependencies=[Depends(verify_header)])
 app.include_router(router)
 
 
 @app.get("/")
-async def index():
-    return {"name": "welcome here"}
+async def index(request: Request):
+    return templates.TemplateResponse(request=request, name="index.html")
