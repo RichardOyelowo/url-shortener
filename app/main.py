60a51bdab7580b200df9cdc152b04dd965e682a6
Richard <richardadebowale.oye@gmail.com>
2026-02-27 19:08:05 -0500

    added admin panel

--- app/main.py
+++ app/main.py
@@ -1,17 +1,30 @@
+from app.database import engine
+from app.models import Link, Click
 from app.utils import verify_header
+from sqladmin import Admin, ModelView
 from app.templates_config import templates
 from fastapi.staticfiles import StaticFiles
 from app.routers import router, admin_router
 from fastapi import FastAPI, Depends, Request
 
-
 app = FastAPI()
-
 app.mount("/static", StaticFiles(directory="app/static"), name="static")
+
+
+# Admin Dashboard
+admin = Admin(app, engine)
+class LinkAdmin(ModelView, model=Link):
+    column_list = [Link.id, Link.original_url, Link.short_code, Link.click_count, Link.created_at]
+
+class ClickAdmin(ModelView, model=Click):
+    column_list = [Click.id, Click.link_id, Click.created_at]
+
+admin.add_view(LinkAdmin)
+admin.add_view(ClickAdmin)
+
 
 app.include_router(admin_router, dependencies=[Depends(verify_header)])
 app.include_router(router)
-
 
 @app.get("/")
 async def index(request: Request):
