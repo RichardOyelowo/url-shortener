22860240a6b26f5e7dd319816c103526a3fa53d1
Richard <richardadebowale.oye@gmail.com>
2026-03-05 02:25:18 -0500

    fix: full admin routes function tests

--- tests/test_admin.py
+++ tests/test_admin.py
@@ -1,20 +1,26 @@
 import pytest
+from app.utils import decode_shortcode
 
 
 @pytest.mark.asyncio
 async def test_admin_all_links(client):
     response = await client.get("/admin/links")
-    assert response.status_code == 200
+    assert "links" in response.json()
 
 
 @pytest.mark.asyncio
 async def test_get_analytics(client):
-    info = await client.post("/links" data={"original_url": "https://devbrain.online"})
+    info = await client.post("/links/", data={"original_url": "https://devbrain.online"})
     short_code = info.text.split("http://test/")[1].split("<")[0]
     response = await client.get(f"/admin/links/{short_code}/analytics/")
-    assert response.status_code == 200
+    assert "analytics" in response.json()
 
 
 @pytest.mark.asyncio
 async def test_delete_link(client):
-    pass
+    info = await client.post("/links/", data={"original_url": "https://businessdashboard.shop"})
+    short_code = info.text.split("http://test/")[1].split("<")[0]
+    id = decode_shortcode(short_code)
+
+    response = await client.delete(f"/admin/links/{id}")
+    assert response.json()["message"] == "Link deleted"
