4b949c82ab93a1a2e11fb1a81cd317e175031d59
Richard <richardadebowale.oye@gmail.com>
2026-03-05 03:04:24 -0500

    fix: updated routes to escape shortcode routes

--- tests/test_admin.py
+++ tests/test_admin.py
@@ -4,7 +4,7 @@ from app.utils import decode_shortcode
 
 @pytest.mark.asyncio
 async def test_admin_all_links(client):
-    response = await client.get("/admin/links")
+    response = await client.get("/api/admin/links")
     assert "links" in response.json()
 
 
@@ -12,7 +12,8 @@ async def test_admin_all_links(client):
 async def test_get_analytics(client):
     info = await client.post("/links/", data={"original_url": "https://devbrain.online"})
     short_code = info.text.split("http://test/")[1].split("<")[0]
-    response = await client.get(f"/admin/links/{short_code}/analytics/")
+    
+    response = await client.get(f"/api/admin/links/{short_code}/analytics/")
     assert "analytics" in response.json()
 
 
@@ -22,5 +23,5 @@ async def test_delete_link(client):
     short_code = info.text.split("http://test/")[1].split("<")[0]
     id = decode_shortcode(short_code)
 
-    response = await client.delete(f"/admin/links/{id}")
+    response = await client.delete(f"/api/admin/links/{id}")
     assert response.json()["message"] == "Link deleted"
