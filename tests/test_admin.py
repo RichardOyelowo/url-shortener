ce7a6325749297e6968d32bd0d6a4f2426219064
Richard <richardadebowale.oye@gmail.com>
2026-03-05 01:51:11 -0500

    fix: import errors & client omission

--- tests/test_admin.py
+++ tests/test_admin.py
@@ -1,14 +1,14 @@
-import pytest_lazyfixture
+import pytest
 
 
 @pytest.mark.asyncio
-async def test_admin_all_links():
+async def test_admin_all_links(client):
     response = await client.get("/admin/links")
     assert response.status_code == 200
 
 
 @pytest.mark.asyncio
-async def test_get_analytics():
+async def test_get_analytics(client):
     info = await client.post("/links" data={"original_url": "https://devbrain.online"})
     short_code = info.text.split("http://test/")[1].split("<")[0]
     response = await client.get("/admin/links/{short_code}/analytics/")
@@ -16,5 +16,5 @@ async def test_get_analytics():
 
 
 @pytest.mark.asyncio
-async def test_delete_link():
+async def test_delete_link(client):
     pass
